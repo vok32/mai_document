897012964bcee008ffcc246d514189ce8325fb2a
Roman Matyushenko <roman.matyushenko.03@mail.ru>
2025-02-26 16:41:24 +0300

    доработка, похож теперь на маи.ру

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     if (username === 'admin' && password === 'admin') {
-        window.location.href = 'dashboard.html';
+        window.location.href = 'dashboard.html'; // Перенаправление на другую страницу
     } else {
         alert('Неверное имя пользователя или пароль');
     }
